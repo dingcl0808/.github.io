7a7b9f1e3e8b950da8de2e6c63b22c7f14293647
dingcl0808 <114585231+dingcl0808@users.noreply.github.com>
2022-10-19 15:38:15 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -9,3 +9,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
   });
 });
+<!--由于对象映射，所以调用test对象等于调用Android映射的对象-->
+function getlocation(){
+    ttJSCore.invoke("getLocation", "", 1);
+}
